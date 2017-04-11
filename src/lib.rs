c0f430f71f5e92b4b4a0fb86615db7e4c823393b
Trevor Bentley <trevor.bentley@autodesk.com>
2017-04-11 16:37:40 +0200

    Added http query builder.  Added seek command.

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,14 @@ pub use webapi::*;
 extern crate rustc_serialize;
 
 pub mod spotify_api {
-    pub const SCOPES: &'static [&'static str] = &["user-read-private", "streaming", "user-read-playback-state"];
+    pub const SCOPES: &'static [&'static str] = &[
+        "user-read-private", "streaming", "user-read-playback-state"
+    ];
     pub const AUTHORIZE: &'static str = "https://accounts.spotify.com/en/authorize";
     pub const TOKEN: &'static str = "https://accounts.spotify.com/api/token";
     pub const DEVICES: &'static str = "https://api.spotify.com/v1/me/player/devices";
     pub const PLAYER_STATE: &'static str = "https://api.spotify.com/v1/me/player";
     pub const PLAY: &'static str = "https://api.spotify.com/v1/me/player/play";
     pub const PAUSE: &'static str = "https://api.spotify.com/v1/me/player/pause";
+    pub const SEEK: &'static str = "https://api.spotify.com/v1/me/player/seek";
 }
