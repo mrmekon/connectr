06fdcaef4349a6d33458b91ef5a56f708af1634d
Trevor Bentley <trevor@trev-minimac.local>
2019-02-01 23:44:52 +0100

    Release {{version}}

---
+++ implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["connectr"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.85/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"connectr/webapi/struct.PlayContextOffset.html\" title=\"struct connectr::webapi::PlayContextOffset\">PlayContextOffset</a>",synthetic:false,types:["connectr::webapi::PlayContextOffset"]},{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.85/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"connectr/webapi/struct.PlayContext.html\" title=\"struct connectr::webapi::PlayContext\">PlayContext</a>",synthetic:false,types:["connectr::webapi::PlayContext"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["connectr"] = {"name":"","dirs":[{"name":"http","dirs":[],"files":["mod.rs"]},{"name":"osx","dirs":[],"files":["mod.rs","rustnsobject.rs"]},{"name":"settings","dirs":[],"files":["mod.rs"]},{"name":"webapi","dirs":[],"files":["mod.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
